468ff7ce1d7943aa30f79fb89f0cb1b52331332e
thangnq29 <thang.nq170@gmail.com>
2025-07-08 10:55:11 +0700

    update code

--- POM_OrangeHRM/pages/base_page.py
+++ POM_OrangeHRM/pages/base_page.py
@@ -1,13 +1,38 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import StaleElementReferenceException
+from utils.config_reader import ConfigReader
 
 class BasePage:
     def __init__(self, driver):
         self.driver = driver
-        self.wait = WebDriverWait(driver, 10)
+        self.timeout = ConfigReader.load_config().get("timeout") 
+
+    def find_element(self, locator):
+        return WebDriverWait(self.driver, self.timeout).until(
+            EC.presence_of_element_located(locator)
+        )
+
+    def wait_for_element_visible(self, locator):
+        return WebDriverWait(self.driver, self.timeout).until(
+            EC.visibility_of_element_located(locator)
+        )
+
+    def wait_for_element_clickable(self, locator):
+        return WebDriverWait(self.driver, self.timeout).until(
+            EC.element_to_be_clickable(locator)
+        )
 
     def click(self, locator):
-        self.wait.until(EC.element_to_be_clickable(locator)).click()
+            element = self.wait_for_element_clickable(locator)
+            element.click()
+            
+    def enter_text(self, locator, text, clear_first=True):
+        element = self.wait_for_element_visible(locator)
+        if clear_first:
+            element.clear()
+        element.send_keys(text)
 
-    def enter_text(self, locator, text):
-        self.wait.until(EC.visibility_of_element_located(locator)).send_keys(text)
+    def get_text(self, locator):
+        element = self.wait_for_element_visible(locator)
+        return element.text
--- POM_OrangeHRM/tests/login_test.py
+++ POM_OrangeHRM/tests/login_test.py
@@ -1,9 +1,14 @@
 from pages.login_page import LoginPage
 from tests.base_test import BaseTest
+from utils.config_reader import ConfigReader
+from selenium.webdriver.common.by import By
+import allure
 
 class TestLogin(BaseTest):
 
+    @allure.story("Valid Login")
+    @allure.severity(allure.severity_level.CRITICAL)
     def test_valid_login(self):
         login_page = LoginPage(self.driver)
-        assert self.driver.title == "OrangeHRM"
-        login_page.do_login("Admin", "admin123")
+        login_page.do_login(ConfigReader.get_username(), ConfigReader.get_password())
+        assert "dashboard" in self.driver.current_url
